22514315f4ca299456776e0015aa10daea46f0a1
yuriwolf <19800714>
2024-06-26 15:53:57 +0900

    最初のコミット

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
+
+export default defineConfig({
+  integrations: [react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+});
