731b64ec0d1e080a8cf71409567facc79d85731b
yuriwolf <19800714>
2024-06-26 18:06:21 +0900

    追加4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import node from '@astrojs/node';
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  server:{
+    host:'0.0.0.0',
+    port: 3000
+  },
   adapter: node({
     mode: 'standalone'
   }),
