f3f9615ef30034dd2ac344277104bbe878f6958c
yuriwolf <19800714>
2024-06-29 11:33:55 +0900

    追加8

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import node from '@astrojs/node';
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  buildOptiions:{
+    site:'https://sakubun-otasuke_leact.onrender.com',
+    sitemap:true,
+  },
   server:{
     host:'0.0.0.0',
     port: 3000
