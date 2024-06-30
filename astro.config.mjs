2ece9fe2013358c978931953e590ca12662e68ef
yuriwolf <19800714>
2024-06-30 17:29:06 +0900

    loaderの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
           return {
             esbuild: {
               loader: {
-                '.js': 'jsx'
+                '.jsx': 'jsx'
               },
             },
             optimizeDeps: {
